5fa2808ff2aa7d9d1d7ca23bf7a9b948d5aef225
Rychel <DLXPMX8@vw.vwgroup.com>
2023-10-09 15:36:33 +0200

    trying to use mlflow

--- src/ml_models/neural_network_train.py
+++ src/ml_models/neural_network_train.py
@@ -4,46 +4,37 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras import layers
 from tensorflow.keras.optimizers import Adam
-from sacred import Experiment
-from sacred.observers import FileStorageObserver
+from mlflow import log_params, log_metrics, log_artifact, start_run
 
-ex = Experiment("nueral_network_experiment")
-ex.observers.append(FileStorageObserver(r'/neural_models'))
-
-@ex.config
-def my_config():
-    epochs_=10
-    batch_size_=64
-    optimizer_='adam'
-    metrics_='accuracy'
-
-@ex.capture
-def prepare_message(message):
-    return message
-
-@ex.main
-def compile_fit_evaluate_model(x_train, x_valid, x_test, y_train, y_valid, y_test, epochs_=10):
-    print(x_train.shape[1])
-    model = Sequential([
-    layers.Dense(64, activation='relu', input_shape=(x_train.shape[1],)),
-    layers.Dense(64, activation='relu'),
-    layers.Dense(1, activation='sigmoid')   # Probability score between 0 and 1
-    ])
+def compile_fit_evaluate_model(x_train, x_valid, x_test, y_train, y_valid, y_test, epochs_=10,
+                               batch_size_=64, optimizer_='adam',metrics_='accuracy'):
     
-    # custom_optimizer = Adam(learning_rate=0.001)
-    # Adam zajebiscie pasuje do dużych modeli a binary_crossentropy do binarnej klasyfikacji, 'adam'
-    model.compile(loss='binary_crossentropy', optimizer=optimizer_, metrics=[f'{metrics_}']) 
+    with start_run():
+        model = Sequential([
+        layers.Dense(64, activation='relu', input_shape=(x_train.shape[1],)),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(1, activation='sigmoid')   # Probability score between 0 and 1
+        ])
+        
+        # custom_optimizer = Adam(learning_rate=0.001)
+        # Adam zajebiscie pasuje do dużych modeli a binary_crossentropy do binarnej klasyfikacji, 'adam'
+        model.compile(loss='binary_crossentropy', optimizer=optimizer_, metrics=[f'{metrics_}']) 
 
-    #epoch - ilosc przejsc po calym datasecie, batch_size - ile wierszy jest branych w jednej iteracji
-    model.fit(x_train, y_train, epochs=epochs_, batch_size=batch_size_, validation_data=(x_valid, y_valid)) 
+        #epoch - ilosc przejsc po calym datasecie, batch_size - ile wierszy jest branych w jednej iteracji
+        model.fit(x_train, y_train, epochs=epochs_, batch_size=batch_size_, validation_data=(x_valid, y_valid)) 
 
-    loss, accuracy = model.evaluate(x_test, y_test)
-    print(f'loss: {loss}')
-    print(f'accuracy: {accuracy}')
+        loss, accuracy = model.evaluate(x_test, y_test)
+        print(f'loss: {loss}')
+        print(f'accuracy: {accuracy}')
 
-    model.save(r'./model_1.h5')
-    ex.add_artifact(r'model_1.h5')
+        # Save the model
+        model.save('neural_models/model_1')
+        
+        # Log parameters and metrics to MLflow
+        log_params({'epochs': epochs_, 'batch_size': batch_size_, 'optimizer': optimizer_, 'metrics': metrics_})
+        log_metrics({'loss': loss, 'accuracy': accuracy})
+        
+        # Log the saved model as an artifact
+        log_artifact('neural_models/model_1', artifact_path='ml_models')
 
-ex.add_source_file(r"C:\Users\dlxpmx8\Desktop\Projekt_AI\meb_process_data_analysis\src\ml_models\neural_network_train.py")
 
-#ex.run()
