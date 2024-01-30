c4bc532d12f3436ba3b45d320419405e0269e29a
Rychel <DLXPMX8@vw.vwgroup.com>
2024-01-30 16:36:37 +0100

    this is the end

--- src/ml_models/xg_boost.py
+++ src/ml_models/xg_boost.py
@@ -75,3 +75,48 @@ def xgb_model(x_train, x_valid, x_test, y_train, y_valid, y_test, model_name,  r
             log_metrics({'recall_nok':recall_nok, 'recall_ok':recall_ok, 'acc_test':accuracy})
         mlflow.end_run()
     return model
+
+def xgb_model_no_grid(x_train, x_valid, x_test, y_train, y_valid, y_test, model_name,  run_name_='standard_run', comment='no comment'):
+
+    mlflow.set_experiment(run_name_)
+    mlflow.xgboost.autolog()
+
+    grid_params = {
+        'learning_rate': 0.275,
+        'max_depth': 7,
+        'colsample_bytree': 0.9,
+        'subsample': 0.8,
+        'min_child_weight': 2,
+        'gamma': 0,
+        'random_state': 1011,
+        'n_estimators': 50,
+        'booster': 'gbtree',
+        'objective': 'binary:logistic'
+    }
+
+
+    rn = f'xgboost_{model_name}_'
+
+    with mlflow.start_run(run_name=rn):
+        model = XGBClassifier(**grid_params)
+        model.fit(x_train, y_train)
+        mlflow.xgboost.log_model(model, "model")
+
+        predictions_proba = model.predict_proba(x_valid)[:, 1].reshape(-1, 1)
+
+        fig = distribution_of_probability_plot(predictions_proba, y_valid)
+        mlflow.log_figure(fig, 'model_probability.png')
+
+        predictions = np.where(predictions_proba > 0.5, 1, 0)
+
+        cm = create_confusion_matrix(y_valid, predictions)
+        mlflow.log_figure(cm, 'confusion_matrix.png')
+
+        recall_ok = recall_score(y_valid, predictions, pos_label=0)
+        recall_nok = recall_score(y_valid, predictions, pos_label=1)
+        accuracy = accuracy_score(y_valid, predictions)
+
+        log_params(grid_params)
+        log_metrics({'recall_nok': recall_nok, 'recall_ok': recall_ok, 'acc_test': accuracy})
+    
+    return model
